1b24ab8719f0425e37e6819a1fac97b3dd04d824
User <User@WIN-4I8V0TPRSH5>
2017-08-25 22:18:13 +0300

    Init commit

---
+++ Task_3/Task_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 50;
+
+            for(int i = a; i <= b; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    continue;
+                }
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }                       
+            }
+            
+            Console.ReadLine();           
+        }
+    }
+}
